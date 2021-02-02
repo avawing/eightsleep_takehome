69201e397bf83a851262626b3539a8ae5ce20d26
Aveline Wingfield <avelinewingfield@gmail.com>
2021-02-02 08:46:31 -0500

    default dogs

--- doggos/src/Dogs/DogCard.jsx
+++ doggos/src/Dogs/DogCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-export default function DogCard() {
+export default function DogCard(props) {
   return (
     <div className="dog_card_outer">
-      <div className="dog_card_inner"></div>
+      <img src={props.dog} className="dog_card_inner"></img>
       <div className="dog_card_footer">
         Dog Breed / Sub Breed
         <button className="dog_card_favorite">
--- doggos/src/Dogs/DogList.jsx
+++ doggos/src/Dogs/DogList.jsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import DogCard from "./DogCard";
 import "./DogStyles.css";
 
 export default function DogList() {
-  let dogs = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let [dogs, setDogs] = useState([]);
+  let doggos = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  useEffect(() => {
+    axios
+      .get(`https://dog.ceo/api/breeds/image/random/20`)
+      .then((res) => {
+        setDogs(res.data.message);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  }, []);
 
   return (
-    <div className="dog_list_container">
-      {dogs.map((dog) => {
-        return <DogCard />;
-      })}
-    </div>
+    <>
+      <div className="dog_list_container">
+        {dogs.length > 0 ? (
+          dogs.map((dog) => {
+            return <DogCard key={dog} dog={dog} />;
+          })
+        ) : (
+          <span />
+        )}
+      </div>
+      <button>Fetch Doggos</button>
+    </>
   );
 }
