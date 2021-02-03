aa0dc748724f2053980e8b255988f76bd8687794
Aveline Wingfield <avelinewingfield@gmail.com>
2021-02-03 18:15:42 -0500

    local storage implemented

--- doggos/src/Dogs/DogCard.jsx
+++ doggos/src/Dogs/DogCard.jsx
@@ -3,6 +3,25 @@ import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 export default function DogCard(props) {
   const [isClicked, setClicked] = useState(false);
+
+  function handleFavorites(link) {
+    // Get the existing data
+    var favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+    if (favorites.length > 0) {
+      for (var i = 0; i < favorites.length; i++) {
+        if (favorites[i] === link) {
+          favorites.splice(i, 1);
+          localStorage.setItem("favorites", JSON.stringify(favorites));
+          return;
+        }
+      }
+    }
+    favorites.push(link);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    return;
+  }
+
   return (
     <div className="dog_card_outer">
       <img src={props.dog} className="dog_card_inner"></img>
@@ -10,12 +29,8 @@ export default function DogCard(props) {
         Dog Breed / Sub Breed
         <button
           className="dog_card_favorite"
-          onClick={(e) => {
-            if (isClicked === true) {
-              setClicked(false);
-            } else {
-              setClicked(true);
-            }
+          onClick={() => {
+            handleFavorites(props.dog);
           }}
         >
           {isClicked ? <FaHeart /> : <FaRegHeart />}
