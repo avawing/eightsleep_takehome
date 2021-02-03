83b32b72a37e3314d8a37a267c3c58ac02c3fa4e
Aveline Wingfield <avelinewingfield@gmail.com>
2021-02-02 20:51:57 -0500

    home button added and integrated

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,11 +1,23 @@
 import SearchBar from "./SearchBar/SearchBar";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
 
 function App() {
+  const history = useHistory();
   return (
     <div className="App">
-      <header className="App-header">Dog Finder</header>
+      <header className="App-header">
+        <button
+          className="app_home_button"
+          onClick={(e) => {
+            history.push("/");
+          }}
+        >
+          <FaHome />
+        </button>
+        Dog Finder
+      </header>
       <Switch>
         <Route path="/:breed/:subbreed">
           <SearchBar />
